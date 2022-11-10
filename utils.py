f3451ca4cbf5a2832f853adba618e4af11344e59
Moonsoo Ra <ravicmoon@gmail.com>
2022-11-10 23:09:28 +0900

    add gen_patches.ipynb file

---
+++ utils.py
@@ -0,0 +1,34 @@
+
+import numpy as np
+
+from skimage.util.shape import view_as_windows
+
+
+def normalize_seismic_data(seismic):
+    min = seismic.min(axis=(1, 2), keepdims=True)
+    max = seismic.max(axis=(1, 2), keepdims=True)
+    return (seismic - min) / (max - min)
+
+
+def get_sliding_wnd_params(img_sz, patch_sz, step):
+    overlap_sz = patch_sz - step
+    img_width, img_height = img_sz
+
+    horizontal_cnt = int(np.ceil((img_width - overlap_sz) / step))
+    new_img_width = step * horizontal_cnt + overlap_sz
+    pad_l = int((new_img_width - img_width) / 2)
+    pad_r = new_img_width - img_width - pad_l
+
+    vertical_cnt = int(np.ceil((img_height - overlap_sz) / step))
+    new_img_height = step * vertical_cnt + overlap_sz
+    pad_t = int((new_img_height - img_height) / 2)
+    pad_b = new_img_height - img_height - pad_t
+
+    return ((pad_t, pad_b), (pad_l, pad_r)), (horizontal_cnt, vertical_cnt)
+
+
+def get_sliding_wnd_patches(img, padding, patch_sz, step):
+    padded = np.pad(img, padding, "reflect")
+    patches = view_as_windows(padded, (patch_sz, patch_sz), step=step)
+    patches = patches.reshape((-1, patch_sz, patch_sz))
+    return patches
